39089409b25a640be6452afed6cee738914ee151
ryan <hu>
2017-10-31 21:54:54 -0400

    initial commit

---
+++ app/src/main/java/com/team223/frc2016/myapplication/Vision.java
@@ -0,0 +1,15 @@
+package com.team223.frc2016.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Vision extends AppCompatActivity
+{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState)
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_vision);
+	}
+}
